3dfc7afd82f582225c9690a350d613754037de28
Kilomelo <weichuyoucai@gmail.com>
2022-08-25 09:55:16 +0800

    exercise

--- Course_1/week2/logicRegression.py
+++ Course_1/week2/logicRegression.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def sigmoid(z):
     """
     Compute the sigmoid of z
@@ -12,6 +14,26 @@ def sigmoid(z):
     s = 1.0 / (1.0 + np.exp(-1.0 * z))
     
     return s
+
+def initialize_with_zeros(dim):
+    """
+    This function creates a vector of zeros of shape (dim, 1) for w and initializes b to 0.
+    
+    Argument:
+    dim -- size of the w vector we want (or number of parameters in this case)
+    
+    Returns:
+    w -- initialized vector of shape (dim, 1)
+    b -- initialized scalar (corresponds to the bias)
+    """
+    
+    w = np.zeros((dim,1))
+    b = 0
+
+    assert(w.shape == (dim, 1))
+    assert(isinstance(b, float) or isinstance(b, int))
+    
+    return w, b
 
 def propagate(w, b, X, Y):
     """
